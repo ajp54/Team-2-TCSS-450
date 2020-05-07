dd37b105081efd86bf5a1d541e048c88bad04845
Vishnevian <vishne@uw.edu>
2020-05-07 15:02:16 -0700

    added settings option, no usage yet

--- app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -24,10 +26,24 @@ public class MainActivity extends AppCompatActivity {
         //
 
         BottomNavigationView navView = findViewById(R.id.nav_view); // Passing each menu ID as a set of Ids because each
-// menu should be considered as top level destinations.
+        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder( R.id.navigation_home, R.id.navigation_weather, R.id.navigation_contacts, R.id.navigation_chat) .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) { //TODO open a settings fragment Log.d("SETTINGS", "Clicked"); return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
