ae5de271e97ce62abaa0259c075329ffa80ad458
Anders <andersped@yahoo.com>
2020-06-03 17:08:20 -0700

    Working on getting the recent messages in the chat cards.

--- app/src/main/java/edu/uw/tcss450/chatapp/ui/chat/chat_room/ChatRoomViewModel.java
+++ app/src/main/java/edu/uw/tcss450/chatapp/ui/chat/chat_room/ChatRoomViewModel.java
@@ -243,7 +243,12 @@ public class ChatRoomViewModel extends AndroidViewModel {
         //code here will run
     }
 
-
+    /**
+     * Get the usernames of all the people in a given chat.
+     *
+     * @param chatId the ID of the chat room you want the names of.
+     * @param jwt user's jwt
+     */
     public void addChatMembers(final int chatId, final String jwt) {
 
         mJwt = jwt;
@@ -286,6 +291,17 @@ public class ChatRoomViewModel extends AndroidViewModel {
         List<ChatMessage> list = getMessageListByChatId(chatId);
         list.add(message);
         getOrCreateMapEntry(chatId).setValue(list);
+
+        //add massage to the chat card in the chat recycler view
+//        if (chatIds != null) {
+//            int index = chatIds.indexOf(chatId);
+//
+//            if (mRoomList.getValue() != null && mRoomList.getValue().get(index) != null) {
+//                mRoomList.getValue().get(index).setRecentMessage(message.getMessage());
+//            }
+//        }
+
+
     }
 
 //    public void addRecentMessages() {
@@ -302,6 +318,10 @@ public class ChatRoomViewModel extends AndroidViewModel {
 //        }
 //    }
 
+    /**
+     * Create a new chat room.
+     * @param jwt the user's jwt
+     */
     public void createChatRoom(String jwt) {
         mJwt = jwt;
         JSONObject params = new JSONObject();
@@ -340,6 +360,12 @@ public class ChatRoomViewModel extends AndroidViewModel {
 
     }
 
+    /**
+     * add someone to an existing chat room.
+     * @param chatId the ID of the room you want to add the person to.
+     * @param username the username or the email of the user.
+     * @param jwt the user's jwt
+     */
     //the user's email will work in place of the username
     public void joinChatRoom(int chatId, String username, String jwt) {
         Log.i("CHATROOM", "user " + username +  " joining room " + chatId);
@@ -379,6 +405,12 @@ public class ChatRoomViewModel extends AndroidViewModel {
                 .addToRequestQueue(request);
     }
 
+    /**
+     * Delete a user from a chat room.
+     * @param chatId the ID of the room the user will be deleted from
+     * @param email the email of the person you want to remove
+     * @param jwt the user's jwt
+     */
     public void deleteChatMember(int chatId, String email, String jwt) {
         Log.i("CHATROOM", "user " + email +  " leaving room " + chatId);
         mJwt = jwt;
@@ -409,6 +441,10 @@ public class ChatRoomViewModel extends AndroidViewModel {
                 .addToRequestQueue(request);
     }
 
+    /**
+     * Creates a message
+     * @param response The JSON object containing the response from the backend.
+     */
     private void handelMessageSuccess(final JSONObject response) {
         List<ChatMessage> list;
         if (!response.has("chatId")) {
@@ -444,6 +480,10 @@ public class ChatRoomViewModel extends AndroidViewModel {
         }
     }
 
+    /**
+     * add the chat rooms to the mRoomList so it can get added to the recycler view
+     * @param response The JSON object containing the response from the backend.
+     */
     private void handelChatIdSuccess(final JSONObject response) {
         List<ChatRoom> list = new ArrayList<ChatRoom>();
         chatIds = new ArrayList<Integer>();
